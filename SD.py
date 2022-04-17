0c6da2b2588f06955af24c4542b3a0ad9b9a1623
Tosin <arabao@tcd.ie>
2022-04-17 17:26:23 +0100

    python

---
+++ SD.py
@@ -0,0 +1,21 @@
+# Project answer to Q3.1
+# Using Pandas to analyse the data
+# Author: Tosin Araba
+
+import pandas as pd
+
+#df = pd.read_csv('Irisdataset.csv')
+Irisdataset = pd.read_csv("Irisdataset.csv")
+#print(Irisdataset)
+#Irisdataset.head()
+#mean = Irisdataset["petal_length"].mean()
+#print(mean)
+#print(df.info())
+
+Aggstats = Irisdataset[["sepal_length", "sepal_width", "petal_length", "petal_width"]].describe()
+print(Aggstats)
+
+
+#This programme calculates the mean, standard deviation, minimum sepal and petal length, minimum 
+#sepal and petal width, maximum sepal and petal length, maximum sepal and petal width and the 
+# the quartile ranges across the four numerical columns.
