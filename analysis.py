1520888d22e27cae4f9ee1fb8bfaad2353708b72
Tosin <arabao@tcd.ie>
2022-04-26 22:08:19 +0100

    .\Analysis.py

--- analysis.py
+++ analysis.py
@@ -46,10 +46,24 @@ import matplotlib.pyplot as plt
 #Answer to Q3.3
 #Scatter plot of each variable pairs
 
-Irisdataset.plot.scatter( x = 'sepal_length', y = 'sepal_width')
-Irisdataset.plot.scatter( x = 'petal_length', y = 'petal_width')
+#Irisdataset.plot.scatter( x = 'sepal_length', y = 'sepal_width')
+#Irisdataset.plot.scatter( x = 'petal_length', y = 'petal_width')
+
+#plt.show()
+
+
+#Answer to Q3.4
+plt.title('Species variable (in cm)')
+
+plt.plot(Irisdataset.sepal_length, Irisdataset.species, label='sepal_length')
+plt.plot(Irisdataset.sepal_width, Irisdataset.species, label='sepal_width')
+plt.plot(Irisdataset.petal_length, Irisdataset.species, label='petal_length')
+plt.plot(Irisdataset.petal_width, Irisdataset.species, label='petal_width')
+
+plt.legend()
 
 plt.show()
+
 
 
 
