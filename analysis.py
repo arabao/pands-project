437a13a5e88b549d6d986e6fd3bfcdf475997e87
Tosin <arabao@tcd.ie>
2022-05-06 19:19:37 +0100

    python

--- analysis.py
+++ analysis.py
@@ -4,65 +4,111 @@
 
 import pandas as pd
 
-Irisdataset = pd.read_csv("Irisdataset.csv")
+df = pd.read_csv("Irisdataset.csv")
+#print(df.head())
 
-#Aggstats = Irisdataset[["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]].describe()
-#print(Aggstats)
+#comment: the above line reads in the Iris dataset and prints the first 5 rows in the terminal
+
+#df.info()
+#comment: this displays basic information about the dataset
+
+Stats = df.describe()
+#print(Stats)
+#Stats.to_csv('Aggstats.csv')
     
-#The above programme calculates the mean, standard deviation, minimum sepal and petal length, minimum 
-#sepal and petal width, maximum sepal and petal length, maximum sepal and petal width and the 
-# the quartile ranges across the four numerical columns.
+#comment: the above programme calculates some basic statistics to include, total number of samples, 
+#the mean, standard deviation, minimum sepal and petal length, minimum 
+#sepal and petal width, maximum sepal and petal length, maximum sepal and petal width and
+#the quartile ranges across the four numerical columns.
+#A summary of the statitstics is then produced as a csv file.
 
-# writes to csv file
-
-#Aggstats.to_csv('Aggstats.csv')
 
 # Answer to Q3.2
 # Histogram of each variable to a png file
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-#Irisdataset.sepal_length.plot.hist()
+#df.sepal_length.plot.hist()
 #plt.xlabel("Sepal length in cm")
 #plt.title("Distribution of sepal length in cm")
 #plt.savefig("sepal_length.png")
 
-#Irisdataset.sepal_width.plot.hist()
+#df.sepal_width.plot.hist()
 #plt.xlabel("Sepal width in cm")
 #plt.title("Distribution of sepal width in cm")
 #plt.savefig("sepal_width.png")
 
-#Irisdataset.petal_length.plot.hist()
+#df.petal_length.plot.hist()
 #plt.xlabel("Petal length in cm")
 #plt.title("Distribution of petal length in cm")
 #plt.savefig("petal_length.png")
 
-#Irisdataset.petal_width.plot.hist()
+#df.petal_width.plot.hist()
 #plt.xlabel("Petal width in cm")
 #plt.title("Distribution of petal width in cm")
 #plt.savefig("petal_width.png")
 
+#comment: the above programs display histograms of variables as per the titles
+#and stores them in png files
 
 #Answer to Q3.3
 #Scatter plot of each variable pairs
 
-#Irisdataset.plot.scatter( x = 'sepal_length', y = 'sepal_width')
-#Irisdataset.plot.scatter( x = 'petal_length', y = 'petal_width')
+colors = ['red', 'blue', 'green']
+species = ['virginica','versicolor','setosa']
+
+#for i in range(3):
+    #x = df[df['species'] == species[i]]
+    #plt.scatter(x['sepal_length'], x['sepal_width'], c = colors[i], label=species[i])
+    #plt.show()
+    #plt.xlabel("Sepal Length")
+    #plt.ylabel("Sepal Width")
+    #plt.legend()
+    #plt.savefig('scatterplot.png')
+        
+#comment: the above program outputs the two variables, sepal length and sepal
+#width into a scatter plot. The 3 Iris species are differented by the colors 
+#assigned. This is saved as a png file. The same program is run below with other 
+#variable pairs.
+
+#for i in range(3):
+    #x = df[df['species'] == species[i]]
+    #plt.scatter(x['petal_length'], x['petal_width'], c = colors[i], label=species[i])
+    #plt.show()
+    #plt.xlabel("Petal Length")
+    #plt.ylabel("Petal Width")
+    #plt.legend()
+    #plt.savefig('scatterplot2.png')
+    
+#for i in range(3):
+     #x = df[df['species'] == species[i]]
+     #plt.scatter(x['petal_length'], x['sepal_length'], c = colors[i], label=species[i])
+     #plt.xlabel("Petal Length")
+     #plt.ylabel("Sepal Length")
+     #plt.legend()
+     #plt.savefig('scatterplot3.png')
+    
+#for i in range(3):
+    #x = df[df['species'] == species[i]]
+    #plt.scatter(x['petal_width'], x['sepal_width'], c = colors[i], label=species[i])
+    #plt.xlabel("Petal Width")
+    #plt.ylabel("Sepal Width")
+    #plt.legend()
+    #plt.savefig('scatterplot4.png')
+    
+    
+#Answer to Q3.4
+#plt.title('Species variable (in cm)')
+
+#plt.plot(Irisdataset.sepal_length, Irisdataset.species, label='sepal_length')
+#plt.plot(Irisdataset.sepal_width, Irisdataset.species, label='sepal_width')
+#plt.plot(Irisdataset.petal_length, Irisdataset.species, label='petal_length')
+#plt.plot(Irisdataset.petal_width, Irisdataset.species, label='petal_width')
+
+#plt.legend()
 
 #plt.show()
-
-
-#Answer to Q3.4
-plt.title('Species variable (in cm)')
-
-plt.plot(Irisdataset.sepal_length, Irisdataset.species, label='sepal_length')
-plt.plot(Irisdataset.sepal_width, Irisdataset.species, label='sepal_width')
-plt.plot(Irisdataset.petal_length, Irisdataset.species, label='petal_length')
-plt.plot(Irisdataset.petal_width, Irisdataset.species, label='petal_width')
-
-plt.legend()
-
-plt.show()
 
 
 
